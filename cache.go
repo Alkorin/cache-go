57ddf0e804cffd1c3f6119af8fa24578e400052e
Thomas Schaffer <loopfz@gmail.com>
2016-08-11 16:01:30 +0200

    Cache delete, refactor unit tests

--- cache.go
+++ cache.go
@@ -12,46 +12,6 @@ type CachedElement struct {
 	Timestamp time.Time
 }
 
-type CachingStrategy interface {
-	CleanupTick() time.Duration
-	IsExpired(*CachedElement) bool
-	IsCleanable(*CachedElement) bool
-	NewCachedElement(*CachedElement, interface{}, error) (*CachedElement, error)
-	ShouldPropagateError(error) bool
-}
-
-type DefaultCachingStrategy struct {
-	expiration time.Duration
-	cleanup    time.Duration
-}
-
-func NewDefaultCachingStrategy(expiration time.Duration, cleanup time.Duration) *DefaultCachingStrategy {
-	return &DefaultCachingStrategy{expiration: expiration, cleanup: cleanup}
-}
-
-func (cs *DefaultCachingStrategy) CleanupTick() time.Duration {
-	return cs.cleanup
-}
-
-func (cs *DefaultCachingStrategy) IsExpired(e *CachedElement) bool {
-	if cs.expiration != 0 {
-		return time.Since(e.Timestamp) >= cs.expiration
-	}
-	return false
-}
-
-func (cs *DefaultCachingStrategy) IsCleanable(e *CachedElement) bool {
-	return cs.IsExpired(e)
-}
-
-func (cs *DefaultCachingStrategy) NewCachedElement(old *CachedElement, v interface{}, e error) (*CachedElement, error) {
-	return &CachedElement{Value: v, Timestamp: time.Now()}, e
-}
-
-func (cs *DefaultCachingStrategy) ShouldPropagateError(err error) bool {
-	return true
-}
-
 type cacheGetterFunc func(interface{}) (interface{}, error)
 
 type cacheQueueElementResult struct {
@@ -230,4 +190,14 @@ func (c *Cache) Get(key string, data interface{}) (interface{}, error) {
 
 	// Return result
 	return result, err
+}
+
+func (c *Cache) Delete(key string) bool {
+	c.cacheMutex.Lock()
+	_, ok := c.cache[key]
+	if ok {
+		delete(c.cache, key)
+	}
+	c.cacheMutex.Unlock()
+	return ok
 }
--- cache_test.go
+++ cache_test.go
@@ -2,50 +2,74 @@ package cache
 
 import (
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func Test(t *testing.T) {
 
+	nbHit := int64(0)
+
 	f := func(v interface{}) (interface{}, error) {
 		time.Sleep(100 * time.Millisecond)
+		atomic.AddInt64(&nbHit, int64(1))
 		return v, nil
 	}
 
 	cache := NewCache(f, NewDefaultCachingStrategy(200*time.Millisecond, 500*time.Millisecond))
 
-	var foo1, foo2, foo3, foo4, bar1, bar2, bar3, bar4 interface{}
+	var foo1, foo2, foo3, foo4, foo5, bar1, bar2 interface{}
 	var wg sync.WaitGroup
 
-	// Concurrent get
-	wg.Add(4)
+	// Sequential get before expiration
+	wg.Add(2)
 	go func() { foo1, _ = cache.Get("foo", "Cached-foo"); wg.Done() }()
 	go func() { bar1, _ = cache.Get("bar", "Cached-bar"); wg.Done() }()
-	go func() { foo2, _ = cache.Get("foo", "Cached-foo"); wg.Done() }()
-	go func() { bar2, _ = cache.Get("bar", "Cached-bar"); wg.Done() }()
 	wg.Wait()
-
-	// Sequential get from cache
 	wg.Add(2)
-	go func() { foo3, _ = cache.Get("foo", "Cached-foo"); wg.Done() }()
-	go func() { bar3, _ = cache.Get("bar", "Cached-bar"); wg.Done() }()
+	go func() { foo2, _ = cache.Get("foo", "Cached-foo2"); wg.Done() }()
+	go func() { bar2, _ = cache.Get("bar", "Cached-bar2"); wg.Done() }()
 	wg.Wait()
 
-	// Wait cache to expire and retry
-	time.Sleep(300 * time.Millisecond)
+	if foo2 != foo1 || bar2 != bar1 {
+		t.Errorf("foo2/bar2 got new values, cache was not used: foo1=%q, foo2=%q, bar1=%q, bar2=%q", foo1, foo2, bar1, bar2)
+	}
+
+	if nbHit != 2 { // foo1, bar1
+		t.Errorf("Should have 2 hits on cache getter func after foo1/bar1, got %d", nbHit)
+	}
+
+	time.Sleep(1 * time.Second) // Sleep to force expiration
+	// Concurrent get (cache queue)
+	wg.Add(2)
+	go func() { foo3, _ = cache.Get("foo", "Cached-foo3"); wg.Done() }()
+	time.Sleep(10 * time.Millisecond) // Sleep to force goroutine scheduling order
+	go func() { foo4, _ = cache.Get("foo", "Cached-foo4"); wg.Done() }()
+	wg.Wait()
+
+	if foo4 != foo3 {
+		t.Errorf("foo4 got new value, cacheQueue error: foo3=%q, foo4=%q", foo3, foo4)
+	}
+
+	if nbHit != 3 { // foo1, bar1, foo3
+		t.Errorf("Should have 3 hits on cache getter func after foo3, got %d", nbHit)
+	}
+
+	// Delete key
+	if ok := cache.Delete("foo"); !ok {
+		t.Errorf("foo key should have been deleted, got %t", ok)
+	}
 	wg.Add(1)
-	go func() { foo4, _ = cache.Get("foo", "Cached-foo"); wg.Done() }()
+	go func() { foo5, _ = cache.Get("foo", "Cached-foo5"); wg.Done() }()
 	wg.Wait()
 
-	// Wait cleanup and retry
-	time.Sleep(1 * time.Second)
-	wg.Add(1)
-	go func() { bar4, _ = cache.Get("bar", "Cached-bar"); wg.Done() }()
-	wg.Wait()
+	if foo5 != "Cached-foo5" {
+		t.Errorf("foo5 got an invalid value: foo5=%q", foo5)
+	}
 
-	if foo1 != "Cached-foo" || bar1 != "Cached-bar" || foo2 != "Cached-foo" || bar2 != "Cached-bar" || foo3 != "Cached-foo" || bar3 != "Cached-bar" || foo4 != "Cached-foo" || bar4 != "Cached-bar" {
-		t.Fatalf("Invalid return, foo1 = %q, bar1 = %q, bar2 = %q, foo2 = %q, bar3 = %q, foo3 = %q", foo1, bar1, foo2, bar2, foo3, bar3)
+	if nbHit != 4 { // foo1, bar1, foo3, foo5
+		t.Errorf("Should have 4 hits on cache getter func after foo5, got %d", nbHit)
 	}
 
 }
